91255f5c20ad2304254d4674438f5c2f541b3da2
raimon <raimon49@hotmail.com>
2015-10-21 21:07:39 +0900

    Add package meta information

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 from setuptools import setup
-setup(name='guestbook')
+
+
+setup(
+    name='raimon49.guestbook',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+    ],
+)
