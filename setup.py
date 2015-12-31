311f67edfae2072760969bc10bf6cd1af9a7b748
raimon <raimon49@hotmail.com>
2015-12-31 16:28:43 +0900

    Impolement new endpoint '/api/' as return JSON

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_file(filename):
 
 setup(
     name='raimon49.guestbook',
-    version='1.0.0',
+    version='1.1.0',
     description='A guestbook web application.',
     long_description=read_file('README.rst'),
     author='raimon49',
